7e7c2c1be304f49209950948822a25b18a59877c
Matthew Mellies <matthewmellies@gmail.com>
2024-09-21 12:55:10 -0600

    add main and footer sections

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
       <header className="App-header">
         <NavBar />
       </header>
+      <main>
+        <p>main</p>
+      </main>
+      <footer>
+        <p>footer</p>
+      </footer>
     </div>
   );
 }
